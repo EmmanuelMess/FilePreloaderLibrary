4bea370ba73a72381ceb826906ba42106be88ecb
Emmanuel M <emmanuelbendavid@gmail.com>
2018-12-21 19:35:10 -0300

    wip robolectric

---
+++ app/src/test/java/com/amaze/filepreloaderlibrary/sampleapp/MainTest.kt
@@ -0,0 +1,9 @@
+package com.amaze.filepreloaderlibrary.sampleapp
+
+import org.robolectric.RobolectricTestRunner
+import org.junit.runner.RunWith
+
+@RunWith(RobolectricTestRunner::class)
+class SandwichTest {
+
+}
